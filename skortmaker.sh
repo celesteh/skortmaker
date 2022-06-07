bcea4654db4053aa89569ade7ddf92a6c7996618
Charles Hutchins <celesteh@gmail.com>
2022-06-07 13:24:28 +0100

    Optional arguments all working now

--- skortmaker.sh
+++ skortmaker.sh
@@ -1,38 +1,79 @@
 #!/usr/bin/bash
 
-text="NEW MEME\nFORMAT"
-size=100
+TEXT=""
+SIZE=100
 
 color1=$(hexdump -n 3 -e '4/4 "%06X" 1 "\n"' /dev/urandom |xargs)
 color2=$(hexdump -n 3 -e '4/4 "%06X" 1 "\n"' /dev/urandom|xargs)
 
 num=$(date +%s)
 
-# see https://www.redhat.com/sysadmin/arguments-options-bash-scripts
-while getopts ":st:" option; do
-   case $option in
-      s) # size
-         size=$OPTARG;;
-      t) # Enter a string
-         text=$OPTARG;;
-     \?) # Invalid option
-         echo "Error: Invalid option"
-         exit;;
+function show_usage() {
+	echo "Usage $0 [options paramaters]"
+	echo ""
+	echo "Options:"
+	echo "-s|-size font size"
+	echo "-t|--text text"
+	echo "-h|--help"
+return 0
+}
+
+# see https://www.golinuxcloud.com/beginners-guide-to-use-script-arguments-in-bash-with-examples/
+while [ ! -z "$1" ];do
+   case "$1" in
+        -h|--help)
+          show_usage
+          exit
+          ;;
+        -t|--text)
+          shift
+          TEXT="$1"
+          ;;
+        -s|--size)
+          shift
+          SIZE="$1"
+          ;;
+        *)
+       echo "Incorrect input provided"
+       show_usage
+       exit
    esac
+shift
 done
 
-stroke=$(($size/50))
+## see https://www.redhat.com/sysadmin/arguments-options-bash-scripts
+#while getopts ":st:" option; do
+#   case $option in
+#      s) # size
+#      	 echo "size $OPTARG"
+#         size=$(($OPTARG));;
+#      t) # Enter a string
+#         text=$OPTARG;;
+#     \?) # Invalid option
+#         echo "Error: Invalid option"
+#         exit;;
+#   esac
+#done
+
+#echo "size is $SIZE $stroke"
+
+if [ $SIZE -lt 1 ]; then
+	SIZE=1
+fi
+
+stroke=$(($SIZE/50))
 if [ $stroke -lt 1 ]; then
 	stroke=1
 fi
+
 
 tmp="temp/blank$num.gif"
 
 echo "$color1 $color2"
 
 convert data/holed.gif -background "#ff000000" -font Open-Sans-Bold  \
-	-strokewidth $stroke -pointsize $size -stroke white -fill black \
-	 -gravity North -annotate 0 "$text" $tmp
+	-strokewidth $stroke -pointsize $SIZE -stroke white -fill black \
+	 -gravity North -annotate 0 "$TEXT" $tmp
 
 convert -size 575x200 xc: +noise Random -separate \
           null: \( xc: +noise Random -separate -threshold 30% -negate \) \
